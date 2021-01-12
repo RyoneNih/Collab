d10876e432e11e25e11bb2106beacfab82c04762
RyoneNih <76864295+RyoneNih@users.noreply.github.com>
2021-01-12 11:04:33 +0800

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,147 @@
+const help = (prefix, botname, oname) => {
+	return `
+「 𝐑𝐘𝐎𝐁𝐎𝐓 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: 𝐑𝐘𝐎𝐍𝐄
+  ❏ Author: RyzRdy
+                    Nazwaa
+                    Farid
+◪ 𝐀𝐁𝐎𝐔𝐓
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  └─ ❏ ${prefix}bugreport
+◪ 𝐌𝐀𝐊𝐄𝐑
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  └─ ❏ ${prefix}quotemaker
+◪ 𝐌𝐄𝐃𝐈𝐀
+  │
+  ├─ ❏ ${prefix}trendtwit
+  └─ ❏ ${prefix}ytsearch
+◪ 𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  └─ ❏ ${prefix}artinama
+◪ 𝐊𝐄𝐑𝐀𝐍𝐆
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  └─ ❏ ${prefix}bisakah
+◪ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃
+  │
+  ├─ ❏ ${prefix}pinterest
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}joox
+  └─ ❏ ${prefix}joox2
+◪ 𝐌𝐄𝐌𝐄
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ 𝐆𝐑𝐎𝐔𝐏
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote
+  ├─ ❏ ${prefix}demote
+  ├─ ❏ ${prefix}tagall
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}listadmins
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+  ├─ ❏ ${prefix}nsfw
+  └─ ❏ ${prefix}ownergroup
+◪ 𝐒𝐎𝐔𝐍𝐃
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ 𝐌𝐔𝐒𝐈𝐂
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ 𝐏𝐑𝐀𝐘
+  │
+  └─ ❏ ${prefix}quran
+◪ 𝐒𝐓𝐀𝐋𝐊
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ 𝐀𝐍𝐈𝐌𝐄
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  └─ ❏ ${prefix}nekonime
+◪ *18+*
+  |
+  ├─ ❏ ${prefix}randomhentai
+  └─ ❏ ${prefix}nsfwneko
+◪ 𝐅𝐔𝐍
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  └─ ❏ ${prefix}glitch
+◪ 𝐈𝐍𝐅𝐎
+  │
+  ├─ ❏ ${prefix}bahasa
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  ├─ ❏ ${prefix}fakta
+  ├─ ❏ ${prefix}infocuaca
+  └─ ❏ ${prefix}covid
+◪ 𝐎𝐖𝐍𝐄𝐑
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  └─ ❏ ${prefix}clearall
+◪ 𝐎𝐓𝐇𝐄𝐑
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}exe
+  ├─ ❏ ${prefix}qrcode
+  └─ ❏ ${prefix}fml
+◪ 𝐎𝐖𝐍𝐄𝐑 𝐋𝐈𝐒𝐓
+  │
+  ├─ ❏ 𝐑𝐘𝐎𝐍𝐄
+  ├─ ❏ 𝐑𝐀𝐃𝐘𝐀
+  └─ ❏𝐍𝐀𝐙𝐖𝐀𝐀
+`
+}
+
+exports.help = help
